a7c3c3682f4398ccf7e5d28f82e0d0f3b70dab4d
hasilka <luciehasilova@gmail.com>
2021-04-13 11:07:51 +0200

    Pškvorky 4_část

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let player = 'circle';
+const playerElm = document.querySelector('#player');
+const btnElm = document.querySelectorAll('.field__cell');
+
+const switchPlayer = () => {
+  if (player === 'circle') {
+    player = 'cross';
+    playerElm.src = 'images/cross.svg';
+  } else {
+    player = 'circle';
+    playerElm.src = 'images/circle.svg';
+  }
+};
+
+btnElm.forEach((cell) => {
+  cell.addEventListener('click', (event) => {
+    if (
+      !event.target.classList.contains('field__cell--circle') &&
+      !event.target.classList.contains('field__cell--cross')
+    ) {
+      if (player === 'circle') {
+        event.target.classList.add('field__cell--circle');
+      } else {
+        event.target.classList.add('field__cell--cross');
+      }
+      event.target.setAttribute('disabled', '');
+      switchPlayer();
+    }
+  });
+});
